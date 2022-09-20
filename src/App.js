e790f356c79f07b0bc993268c5ec5eeebfb234ac
JATIN'S MACHINE <jatinjaiswal56@gmail.com>
2022-09-20 23:19:24 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    advice: "",
+
+  };
+  componentDidMount() {
+    //this.fetchadvice();
+    this.setState({advice:"HOLLA USER"})
+  }
+  fetchadvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const {advice}=response.data.slip;
+        console.log(advice);
+        this.setState({advice:advice})
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  componentDidUpdate() {}
+ componentWillUnmount() {}
+
+  render() {
+    return(
+      <div className="app">
+      <div className="card">
+      <h1 className="heading">{this.state.advice}</h1>
+      <button className="button" onClick={this.fetchadvice}><span>GIVE ME ADVICE!</span></button>
+      </div>
+    </div>
+    ) 
+   
+  }
+}
+
+export default App;
