fc39b5d47d7856ab062e357d8ac26d2eb78dbd7a
JATIN'S MACHINE <jatinjaiswal56@gmail.com>
2022-09-20 23:38:01 +0530

    TESTING

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from "./logo.svg";
 import React from "react";
 import axios from "axios";
 import "./App.css";
-
+//commit
 class App extends React.Component {
   state = {
     advice: "",
+
 
   };
   componentDidMount() {
